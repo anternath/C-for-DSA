c3bc25fdb57d73df9fe1c16d964378ee95a04b0e
Anter Nath <anternath73@gmail.com>
2024-10-29 01:38:57 +0600

    how to output in C++

---
+++ output.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    int a=500;
+    float f= 29.837387384;
+    long long int lla= 29474734;
+    double lf=29.38439058;
+    char ch= 'A';
+    std ::cout<<"print int num: "<<a<<"\n";
+    std ::cout<<"print float: "<<f<<" & double : "<<lf<< std ::endl;
+    std :: cout<<"print character : "<<ch;
+    return 0;
+}
